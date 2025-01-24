68079b772864675b9964c18e52f6f6854b825c7f
Ubuntu <ubuntu@ubuntu2004.linuxvmimages.local>
2025-01-23 20:50:34 -0500

    Primer paquete creado

---
+++ mi_primer_paquete/src/publicador.cpp
@@ -0,0 +1,31 @@
+#include "ros/ros.h"
+#include "geometry_msgs/Twist.h"
+
+int main(int argc, char **argv){
+
+  ros::init(argc, argv, "nodo_publicado"); /*nombre del publicador*/
+
+  ros::NodeHandle n;
+
+  ros::Publisher cmd_vel_pub = n.advertise<geometry_msgs::Twist>("turtle1/cmd_vel", 10);
+
+  ros::Rate loop_rate(10);
+
+  //geometry_msgs::msg::Twist cmd_vel_msg;
+  geometry_msgs::Twist cmd_vel_msg;
+
+  cmd_vel_msg.linear.x = 0.5;
+  cmd_vel_msg.angular.z = 0.5;
+
+  
+  int count = 0;
+  while (ros::ok())
+  {
+    std::cout<< "Publicando velocidad en tortuga" <<  std::endl;
+    //cmd_vel_pub.publish(cmd_vel_pub);
+    cmd_vel_pub.publish(cmd_vel_msg);
+    loop_rate.sleep();
+    
+  }
+  return 0;
+}
